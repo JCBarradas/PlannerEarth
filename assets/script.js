8edf488e49d07fc57506ecf960e3db0770ab7167
Jose Barradas <150345330+JCBarradas@users.noreply.github.com>
2024-01-19 23:36:12 +0000

    added and interval to update the current time every hour, updated the background color of the time blocks and saved the colors on the local storage so they dont despear when the page is refreshed

--- assets/script.js
+++ assets/script.js
@@ -5,59 +5,92 @@
 //Save the event in local storage when the save button is clicked in that timeblock.
 //Persist events between refreshes of a page
 
-$(document).ready(function () {
-    // Display the current day
-    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
-  
-    // Generate timeblocks 
-   var timeblocksContainer = $(".container");
-  
-    for (var i = 9; i <= 19; i++) {
-      var timeblock = $("<div>")
-        .addClass("row time-block")
-        .attr("data-hour", i);
-  
-      var hourCol = $("<div>").addClass("col-1 hour").text(`${i}:00`);
-      var textAreaCol = $("<textarea>").addClass("col-10 description");
-  
-      // Load events from local storage on page load
-      var savedEvent = localStorage.getItem(`event-${i}`);
-      if (savedEvent) {
-        textAreaCol.val(savedEvent);
-        console.log(savedEvent)
+// Use a flag to check if the script has already run
+var scriptExecuted = false;
+
+// Use a flag to check if the script has already run
+var scriptExecuted = false;
+
+// Check if the script has already run
+if (!scriptExecuted) {
+    $(document).ready(function () {
+        // Set the flag to true to indicate that the script has been executed
+        scriptExecuted = true;
+
+        // Display the current day
+        $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+
+        // Generate timeblocks 
+        var timeblocksContainer = $(".container");
+
+        // Clear existing content inside the container
+        timeblocksContainer.empty();
+
+        for (var i = 9; i <= 19; i++) {
+            var timeblock = $("<div>")
+                .addClass("row time-block")
+                .attr("data-hour", i);
+
+            var hourCol = $("<div>").addClass("col-1 hour").text(`${i}:00`);
+            var textAreaCol = $("<textarea>").addClass("col-10 description");
+
+            // Load events from local storage on page load
+            var savedEvent = localStorage.getItem(`event-${i}`);
+            if (savedEvent) {
+                textAreaCol.val(savedEvent);
+            }
+
+            var saveBtnCol = $("<button>")
+                .addClass("col-1 saveBtn")
+                .html('<i class="fas fa-save"></i>');
+
+            timeblock.append(hourCol, textAreaCol, saveBtnCol);
+            timeblocksContainer.append(timeblock);
+
+            // Color-code timeblocks based on whether they are before, at, or after the present time
+            updateColors(timeblock, i);
+        }
+
+        // Attach event listener to save buttons for entering events
+        $(".saveBtn").on("click", function () {
+            var hour = $(this).parent().attr("data-hour");
+            var eventText = $(this).siblings(".description").val();
+            // Save the event in local storage
+            localStorage.setItem(`event-${hour}`, eventText);
+            // Update colors after saving event
+            updateColors($(this).parent(), hour);
+        });
+
+        // Set colors based on the information stored in local storage
+        for (var i = 9; i <= 19; i++) {
+            updateColors($(`[data-hour=${i}]`), i);
+        }
+
+        // Update time every 60 minutes
+        setInterval(function () {
+            $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+            for (var i = 9; i <= 19; i++) {
+                updateColors($(`[data-hour=${i}]`), i);
+            }
+        }, 60 * 60 * 1000);
+
+        function updateColors(timeblock, hour) {
+          var currentHour = dayjs().hour();
+      
+          if (hour < currentHour) {
+              timeblock.removeClass("present future").addClass("past").css(".past");
+              // Save the color information to local storage
+              localStorage.setItem(`color-${hour}`, "past");
+          } else if (hour === currentHour) {
+              timeblock.removeClass("past future").addClass("present");
+              // Save the color information to local storage
+              localStorage.setItem(`color-${hour}`, "present");
+          } else {
+              timeblock.removeClass("past present").addClass("future");
+              // Save the color information to local storage
+              localStorage.setItem(`color-${hour}`, "future");
+          }
       }
-
-      var saveBtnCol = $("<button>")
-      .addClass("col-1 saveBtn")
-      .html('<i class="fas fa-save"></i>');
-
-    timeblock.append(hourCol, textAreaCol, saveBtnCol);
-    timeblocksContainer.append(timeblock);
-
-    // Color-code timeblocks based on past, present, and future
-    updateColors(timeblock, i);
-  }
-
-  // Attach event listener to save buttons for entering events
-  $(".saveBtn").on("click", function () {
-    var hour = $(this).parent().attr("data-hour");
-    var eventText = $(this).siblings(".description").val();
-    // Save the event in local storage
-    localStorage.setItem(`event-${hour}`, eventText);
-    // Update colors after saving event
-    updateColors($(this).parent(), hour);
-  });
-  
-  function updateColors(timeblock, hour) {
-    var currentHour = dayjs().hour();
-
-    if (hour < currentHour) {
-      timeblock.removeClass("present future").addClass("past");
-    } else if (hour === currentHour) {
-      timeblock.removeClass("past future").addClass("present");
-    } else {
-      timeblock.removeClass("past present").addClass("future");
-    }
-  }
-  
-});
+       
+    });
+}
